813a8ae38d1a530db295e90726ac9d4517ecb7ca
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-06-12 01:18:28 +0300

    Update lab5.js

--- lab5.js
+++ lab5.js
@@ -3,28 +3,28 @@ const divideFn = size => key => key % size
 const multiplyFn = size => key => Math.floor(size * (key * 0.268124 % 1))
 
 class HashTable {
-  constructor(size, hashFn) {
-    this.size = size
-    this.fn = hashFn(size)
-    this.items = new Array(size).fill(0).map(() => [])
-	}
-  add(key, value) {
-    if(this.has(key)) return console.log(`Помилка: елемент з ключем ${key} вже існує`)
-    this.items[this.fn(key)].push([key, value])
-	} 
-	remove(key) {
-	  if(!this.has(key)) return console.log(`Помилка: елементу з ключем ${key} не існує`)
-	  this.items[this.fn(key)] = this.items[this.fn(key)].filter(item => item[0] !== key)
-	 }
-	 
-  has(key) {
-	  return this.items[this.fn(key)].filter(item => item[0] === key).length !== 0
-	  }
-  get(key) {
-	 if(!this.has(key)) return console.log(`Елементу з ключем ${key} не існує`)
-   return this.items[this.fn(key)].find(item => item[0] === key)[1]
-  }
-	}
+    constructor(size, hashFn) {
+        this.size = size
+        this.fn = hashFn(size)
+        this.items = new Array(size).fill(0).map(() => [])
+    }
+    add(key, value) {
+        if (this.has(key)) return console.log(`Помилка: елемент з ключем ${key} вже існує`)
+        this.items[this.fn(key)].push([key, value])
+    }
+    remove(key) {
+        if (!this.has(key)) return console.log(`Помилка: елементу з ключем ${key} не існує`)
+        this.items[this.fn(key)] = this.items[this.fn(key)].filter(item => item[0] !== key)
+    }
+
+    has(key) {
+        return this.items[this.fn(key)].filter(item => item[0] === key).length !== 0
+    }
+    get(key) {
+        if (!this.has(key)) return console.log(`Елементу з ключем ${key} не існує`)
+        return this.items[this.fn(key)].find(item => item[0] === key)[1]
+    }
+}
 
 
          
